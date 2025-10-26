91f0b04b218e925735a34b09719ec3d50a68efac
aimeeflow <nugrohoaimee@gmail.com>
2025-10-27 05:58:44 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hi! This is Aimee trying to work on Mobile App Backend!"
+
+@app.route('/api/products')
+def products():
+    data = [
+        {"id": 1, "nama": "Aimee", "email": "aimee@mail.com"},
+        {"id": 2, "nama": "Juni", "email": "juni@mail.com"},
+        {"id": 3, "nama": "Nindy", "email": "nindy@mail.com"}
+    ]
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
